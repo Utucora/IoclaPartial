7532383f1e45dab31174caf0129eab127e1b5e7f
Utucora <166538040+Utucora@users.noreply.github.com>
2025-05-09 17:28:50 +0300

    Am facut reverse-array

--- labs/lab-07/tasks/reverse-array/support/reverse-array.asm
+++ labs/lab-07/tasks/reverse-array/support/reverse-array.asm
@@ -14,7 +14,29 @@ global main
 main:
 
     ; TODO push the elements of the array on the stack
+    xor edi, edi
+    mov edi, input
+    mov ecx, 7
+loop_push:
+    push dword[edi]
+    dec ecx
+    cmp ecx, 0
+        je finish_push
+    add edi, 4
+    jmp loop_push
+finish_push:
     ; TODO retrieve the elements (pop) from the stack into the output array
+mov edi, output
+mov ecx, 7
+loop_pop:
+    pop dword[edi]
+    dec ecx
+    cmp ecx, 0
+        je finish_prog
+    add edi, 4
+    jmp loop_pop
+
+finish_prog:
 
     PRINTF32 `Reversed array: \n\x0`
     xor ecx, ecx
